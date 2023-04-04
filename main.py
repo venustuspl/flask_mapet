4a3e23a755b823536e54659a5d18266b84177c15
Tomasz Kostro <tomasz.kostro@marketplanet.pl>
2023-04-04 08:06:31 +0200

    20230404DebugMode

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 
-def print_hi(name):
-    print(f'Hi, {name}')
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return '<h1>Hello, World!</h1>'
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    app.run(debug = True, port = 80);
 
+# Zmieniam na 80 by móc wywoływać aplikację  po prostu „localhost” zamiast „localhost:5000”.
+# Parametr „debug” powoduje, że zmiany na plikach z kodem będą od razu aktualizowane na serwerze, bez potrzeby restartu.
